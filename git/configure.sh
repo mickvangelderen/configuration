bf9b17f2f57589f99efe7f6f7f9ff1335bff83e3
Mick van Gelderen <mickvangelderen@gmail.com>
2016-03-03 13:39:49 +0100

    Remove -n flag from git/configure core editor

--- git/configure.sh
+++ git/configure.sh
@@ -3,4 +3,4 @@
 git config --global core.excludesfile "$MICK_CONFIGURATION_FOLDER/git/.gitignore"
 git config --global user.name "Mick van Gelderen"
 git config --global user.email "mickvangelderen@gmail.com"
-git config --global core.editor "subl -n -w"
+git config --global core.editor "subl -w"
