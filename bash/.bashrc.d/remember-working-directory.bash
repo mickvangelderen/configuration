e10ed2939437a4b19065d0888026c2cd51741a98
Mick van Gelderen <mickvangelderen@gmail.com>
2018-01-30 01:11:23 +0100

    Remove remember-working-directory.bash

--- bash/.bashrc.d/remember-working-directory.bash
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ ! -z "$PROMPT_COMMAND" ]; then
-	PROMPT_COMMAND+='; '
-fi
-PROMPT_COMMAND+='printf %s "$PWD" > ~/restorable-working-directory.txt'
-cd "$(<~/restorable-working-directory.txt)"
