cd33bd0b5c723b4da3b4431df7f0f13710052219
Mick van Gelderen <mickvangelderen@gmail.com>
2018-10-24 11:32:39 +0200

    Update disable mouse accel for libinput

--- ubuntu/disable-mouse-acceleration.bash
+++ ubuntu/disable-mouse-acceleration.bash
@@ -1,11 +1,22 @@
 #!/usr/bin/env bash
 
+# If the flat accelprofile is not supported we can use this.
+# cat <<END >> /usr/share/X11/xorg.conf.d/50-mouse-acceleration.conf
+# Section "InputClass"
+# 	Identifier "My Mouse"
+# 	MatchIsPointer "yes"
+# 	Option "AccelerationProfile" "-1"
+# 	Option "AccelerationScheme" "none"
+# 	Option "AccelSpeed" "-1"
+# EndSection
+# END
+
 cat <<END >> /usr/share/X11/xorg.conf.d/50-mouse-acceleration.conf
 Section "InputClass"
-    Identifier "My Mouse"
-    MatchIsPointer "yes"
-    Option "AccelerationProfile" "-1"
-    Option "AccelerationScheme" "none"
-    Option "AccelSpeed" "-1"
+	Identifier "Mick Mouse"
+	Driver "libinput"
+	MatchIsPointer "yes"
+	Option "AccelProfile" "flat"
+	Option "AccelSpeed" "0"
 EndSection
 END
