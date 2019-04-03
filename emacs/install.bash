93b4456171e6e7105746c1932aa0ca42bce63512
Mick van Gelderen <mickvangelderen@gmail.com>
2019-04-03 09:24:30 +0200

    Checkout spacemacs config develop

--- emacs/install.bash
+++ emacs/install.bash
@@ -6,4 +6,4 @@ sudo apt-get install emacs25
 
 git clone https://github.com/syl20bnr/spacemacs --branch develop ~/.emacs.d
 
-git clone git@github.com:mickvangelderen/.spacemacs.d ~/.spacemacs.d
+git clone git@github.com:mickvangelderen/.spacemacs.d --branch develop ~/.spacemacs.d
