632eb709689ef8153f81b52839c229d00413579b
Mick van Gelderen <mickvangelderen@gmail.com>
2021-01-24 23:55:09 +0100

    Update source code pro font version

--- ubuntu/install-font-source-code-pro.bash
+++ ubuntu/install-font-source-code-pro.bash
@@ -11,7 +11,7 @@ echo "Installing source code pro to $INSTALL_DIR"
 sudo mkdir -p "$INSTALL_DIR"
 
 (sudo git clone \
-   --branch "2.030R-ro/1.050R-it" \
+   --branch "2.038R-ro/1.058R-it/1.018R-VAR" \
    --depth 1 \
    'https://github.com/adobe-fonts/source-code-pro.git' \
    "$INSTALL_DIR" && \
