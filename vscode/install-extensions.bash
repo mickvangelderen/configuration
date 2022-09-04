792e64735b7fb918c44096e594b90449444a1716
Mick van Gelderen <mickvangelderen@gmail.com>
2022-09-04 10:17:58 +0200

    Update vscode extensions

--- vscode/install-extensions.bash
+++ vscode/install-extensions.bash
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-code --install-extension "ms-vscode.cpptools"
-code --install-extension "wmaurer.change-case"
-code --install-extension "rust-lang.rust"
-code --install-extension "tyriar.sort-lines"
-code --install-extension "tomoki1207.vscode-input-sequence"
-
+code --install-extension "eamodio.gitlens"
+code --install-extension "ms-python.python"
+code --install-extension "rust-lang.rust-analyzer"
+code --install-extension "yzhang.markdown-all-in-one"
