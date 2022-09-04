1e229d5d7636996f25d1e3f2c30b9347b379aa34
Mick van Gelderen <mickvangelderen@gmail.com>
2022-09-04 10:29:25 +0200

    Update git config

--- git/configure.sh
+++ git/configure.sh
@@ -5,6 +5,8 @@ git config --global user.email "mickvangelderen@gmail.com"
 
 git config --global core.editor "vim"
 
+git config --global init.defaultBranch "master"
+
 git config --global alias.pf "push --force-with-lease"
 git config --global alias.po "!git push --set-upstream origin \"\$(git branch --show-current)\""
 git config --global alias.ca "commit --amend --reuse-message"
