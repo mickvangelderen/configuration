95189d4c4f77a06ee0c03741382b9f45af7be01f
Mick van Gelderen <mickvangelderen@gmail.com>
2024-03-05 12:19:53 +0100

    Add more git aliases

--- git/configure.sh
+++ git/configure.sh
@@ -7,7 +7,10 @@ git config --global core.editor "vim"
 
 git config --global init.defaultBranch "master"
 
+git config --global alias.p "push"
 git config --global alias.pf "push --force-with-lease"
 git config --global alias.po "!git push --set-upstream origin \"\$(git branch --show-current)\""
-git config --global alias.ca "commit --amend --reuse-message"
-git config --global alias.rom "!git fetch && git rebase --interactive origin/master"
+git config --global alias.ca "commit --amend --reuse-message --no-edit"
+git config --global alias.cae "commit --amend --reuse-message"
+git config --global alias.rom "!git fetch && git ls-remote --exit-code --heads origin master >/dev/null 2>&1 && git rebase --interactive origin/master || git rebase --interactive origin/main"
+git config --global alias.rum "!git fetch && git ls-remote --exit-code --heads upstream master >/dev/null 2>&1 && git rebase --interactive upstream/master || git rebase --interactive upstream/main"
