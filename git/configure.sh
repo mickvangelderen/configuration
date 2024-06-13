948d15337a8bb7307558b37e41c0f49f1720bc29
Mick van Gelderen <mickvangelderen@gmail.com>
2024-06-13 13:59:23 +0200

    Rework git aliases

--- git/configure.sh
+++ git/configure.sh
@@ -7,10 +7,18 @@ git config --global core.editor "vim"
 
 git config --global init.defaultBranch "master"
 
-git config --global alias.p "push"
-git config --global alias.pf "push --force-with-lease"
-git config --global alias.po "!git push --set-upstream origin \"\$(git branch --show-current)\""
-git config --global alias.ca "commit --amend --reuse-message --no-edit"
-git config --global alias.cae "commit --amend --reuse-message"
-git config --global alias.rom "!git fetch && git ls-remote --exit-code --heads origin master >/dev/null 2>&1 && git rebase --interactive origin/master || git rebase --interactive origin/main"
-git config --global alias.rum "!git fetch && git ls-remote --exit-code --heads upstream master >/dev/null 2>&1 && git rebase --interactive upstream/master || git rebase --interactive upstream/main"
+git config --global alias.default-branch '!sed -e '\''s/^.*\///'\'' < .git/refs/remotes/origin/HEAD'
+git config --global alias.p 'push'
+git config --global alias.pf 'push --force-with-lease'
+git config --global alias.po '!git push --set-upstream origin "$(git branch --show-current)"'
+git config --global alias.c 'commit'
+git config --global alias.wip 'commit -m WIP'
+git config --global alias.awip 'commit -a -m WIP'
+git config --global alias.ca 'commit --amend --no-edit'
+git config --global alias.cae 'commit --amend'
+git config --global alias.r 'rebase --interactive'
+git config --global alias.new '!git fetch && git switch --create "$1" "origin/$(git default-branch)" #'
+git config --global alias.rom '!git fetch && git rebase --interactive "origin/$(git default-branch)"'
+git config --global alias.rum '!git fetch && git rebase --interactive "upstream/$(git default-branch)"'
+git config --global alias.plb "!git fetch --all --prune && for branch in $(git for-each-ref --format '%(refname) %(upstream:track)' refs/heads | awk '$2 == "[gone]" {sub("refs/heads/", "", $1); print $1}'); do git branch -D $branch; done"
+
